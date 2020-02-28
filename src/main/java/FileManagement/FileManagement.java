d62b70be31cee6d5a05d56a5462e55d98b3e59d3
Vo Minh Hoang <hoangvm.hiephoa@gmail.com>
2020-02-28 18:04:30 +0700

    First commit. Bugs in main

---
+++ src/main/java/FileManagement/FileManagement.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FileManagement;
+
+import java.io.File;
+import java.util.Scanner;
+/**
+ *
+ * @author vomin
+ */
+public class FileManagement {
+    
+    public static Scanner scanner = new Scanner(System.in);
+    private String pathFile;
+    //---Constructor & Getter Setter
+    public FileManagement(){
+    }
+    public FileManagement(String pathFile){
+        this.pathFile=pathFile;
+    }
+    public String getPath(){
+        return this.pathFile;
+    }
+    public void setPath(String Path){
+        this.pathFile=Path;
+    }
+    //---Create New Folder
+    public void createNewFolder() {
+        FileManagement file = new FileManagement();
+        file.pathFile=file.inputPath();
+        System.out.println(file.pathFile);
+        File apath = new File(pathFile);
+        if (!apath.exists()){
+            apath.mkdirs();
+            System.out.println("Create new folder successfully !!!");
+        }
+        else{
+            System.out.println("Folder exists !!!");
+        }
+    }
+    //---Private
+    private String inputPath(){
+        System.out.print("Input the File Path : ");
+        return scanner.nextLine();
+    }
+}
---
+++ src/main/java/Main_Class/Main_Class.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main_Class;
+import FileManagement.FileManagement;
+/**
+ *
+ * @author vomin
+ */
+public class Main_Class {
+    public static void main(String[] agrs){
+        FileManagement file = new FileManagement();
+        file.createNewFolder();
+    }
+}
